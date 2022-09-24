22b909ce62f4f53304db7b53c79fc2e0425011bd
RFeno <skaytroz@gmail.com>
2022-09-24 14:01:43 +0200

    Ajout de menu principale et correction db

--- bdd.py
+++ bdd.py
@@ -33,13 +33,13 @@ cursor = connexion.cursor()
 
 #creation des requetes
 requeteCreationUser = "create table users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,username varchar(255) unique NOT NULL, password varchar(255) NOT NULL);" 
-requeteCreationClass = "create table class (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,className varchar(255) unique NOT NULL, numberOfMachines integer , numberOfNetwork INTEGER, mask varchar(255) not null);"
+requeteCreationClass = "create table class (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,className varchar(255) unique NOT NULL, numberOfMachines varchar(255) , numberOfNetwork INTEGER, mask varchar(255) not null);"
 
 
 #exécution des requetes
 cursor.execute(requeteCreationUser)
 cursor.execute(requeteCreationClass)
-cursor.execute("insert into class (className,numberOfMachines,numberOfNetwork,mask) values ('A',126,16777214,'255.0.0.0'),('B',16384,65534,'255.255.0.0'),('C',2097152,256,'255.255.255.0'),('D',0,0,'pas de masque : classe du multicast (multi-diffusion)'),('E',0,0,'pas de masque : classe des expérimentations'),('Réservées',0,0,'');")
+cursor.execute("insert into class (className,numberOfMachines,numberOfNetwork,mask) values ('A',126,16777214,'255.0.0.0'),('B',16384,65534,'255.255.0.0'),('C',2097152,256,'255.255.255.0'),('D','adresses uniques',0,'pas de masque : classe du multicast (multi-diffusion)'),('E','adresses uniques',0,'pas de masque : classe des expérimentations'),('Réservées','Classes réservées',0,'');")
 cursor.execute('insert into users (username,password) values ("Saviour",?),("Boulogne",?),("Alexandre",?);',(hashSaviour.decode(),hashBoulogne.decode(),hashAlexandre.decode()))
 
 
--- menu.py
+++ menu.py
@@ -1,31 +1,148 @@
+from ast import In
 from tkinter import *
 from tkinter import ttk
+from tkinter.ttk import *
+import tkinter.font as font
+
+"""Fenêtre Principale"""
+#création de la fenêtre principale
+WindowMain = Tk()
+
+#ajout du titre de la fenêtre
+WindowMain.title("Projet LABO_TCPIP - Menu d'accueil")
+
+#taille minimale de la enêtre
+WindowMain.minsize(500,500)
+
+#ajout de l'icone (iamge)
+WindowMain.iconbitmap("ressources/images/logoLabo4_1.ico")
+
+#configuration visuelle de la fenêtre
+WindowMain.config(background="#009790")
+styleMain = Style()
+styleMain.configure('My.TFrame', background="#009790", font="Impact")
+"""Fin Fenêtre Principale"""
+
+#fonctions
+
+"""Créations des composants"""
+frameMain = Frame(WindowMain, padding=30, style='My.TFrame')
+framePoint1 = Frame(WindowMain, padding=30,style='My.TFrame')
+framePoint2 = Frame(WindowMain, padding=30,style='My.TFrame')
+framePoint3 = Frame(WindowMain, padding=30,style='My.TFrame')
+framePoint4 = Frame(WindowMain, padding=30,style='My.TFrame')
+framePoint5 = Frame(WindowMain, padding=30,style='My.TFrame')
+
+"""Fin création des composants"""
+
+def HiddenAllWindow():
+    frameMain.grid_forget()
+    framePoint1.grid_forget()
+    framePoint2.grid_forget()
+    framePoint3.grid_forget()
+    framePoint4.grid_forget()
+    framePoint5.grid_forget()
+
+    
+def displayPoint1():
+    
+    #cacher les fenêtre
+    HiddenAllWindow()
+    
+    #changement titre
+    WindowMain.title("Projet LABO_TCPIP - Point 1")
+    
+    #ajout du conteneur
+    framePoint1.grid()
+    
+    #création des composants
+    ttk.Label(framePoint1, text="1.Salut mec").grid(column=0, row=1)
+    ttk.Button(framePoint1, text="Valider (point 1)", ).grid(column=2, row=1)
 
 
-root = Tk()
-frm = ttk.Frame(root, padding=30)
-frm.grid()
+    
+def displayAbout():
+    print("A propos")
+    
+def displayMenuMain():
+    
+    #cacher les fenêtre
+    HiddenAllWindow()
+    
+    #changement titre
+    WindowMain.title("Projet LABO_TCPIP - Menu d'accueil")
+    
+    #création des sections du menu
+    frameMenuPoint1 = Frame(frameMain,style='My.TFrame')
+    frameMenuPoint2 = Frame(frameMain,style='My.TFrame')
+    frameMenuPoint3 = Frame(frameMain,style='My.TFrame')
+    frameMenuPoint4 = Frame(frameMain,style='My.TFrame')
+    frameMenuPoint5 = Frame(frameMain,style='My.TFrame')
+    myFont = font.Font(family='Helvetica')
+    
+    #Point 1 composants
+    Label(frameMenuPoint1, text="1. Trouver les informations de l'adresse IP                      ",foreground="white",font=("Impact",15),background="#009790").grid(column=0, row=0)
+    Label(frameMenuPoint1, text="A. Classe \nB. Nombre de réseaux \nC. Nombre de machines                                                             ",foreground="white",font=("Arial",10),background="#009790").grid(column=0, row=1)
+    Button(frameMain, text="Accéder (point 1)",command=displayPoint1).grid(column=5, row=1)
+  
+   
 
-ttk.Label(frm, text="1. Trouver les informations de l'adresse IP\n     A. Classe \n     B. Nombre de réseaux \n     C. Nombre de machines").grid(column=0, row=1)
-ttk.Button(frm, text="Valider (point 1)", ).grid(column=2, row=1)
+    #Point 2 composants
+    Label(frameMenuPoint2, text="2. Trouver les informations de l'adresse IP                     ",foreground="white",font=("Impact",15),background="#009790").grid(column=0, row=3)
+    Label(frameMenuPoint2, text="A. Adresse de réseau\nB. Adresse de broadcast\nC. Adresse de sous-réseau                                                        ",foreground="white",font=("Arial",10),background="#009790").grid(column=0, row=4)
+    Button(frameMain, text="Accéder (point 2)").grid(column=5, row=2)
+
+    #Point 3 composants
+    Label(frameMenuPoint3, text="3. L'IP apartient-il au réseau ?                                              ",foreground="white",font="Impact",background="#009790").grid(column=0, row=6)
+    Button(frameMain, text="Accéder (point 3)").grid(column=5, row=3)
+
+    #Point 4 composants
+    Label(frameMenuPoint4, text="4. Deux adresses sont-elles dans le même réseau ?   ",foreground="white",font="Impact",background="#009790").grid(column=0, row=9)
+    Button(frameMain, text="Accéder (point 4)").grid(column=5, row=4)
+
+    Label(frameMenuPoint5, text="5. Déterminer les possibilités                                                ",foreground="white",font="Impact",background="#009790").grid(column=0, row=12)
+    Label(frameMenuPoint5, text="A. Le nombre d'hôtes\nB. Découpe sur nombre de sous-réseaux \nC. Découpe par nombre d'adresse IP                                           ",foreground="white",font=("Arial",10),background="#009790").grid(column=0, row=13)
+    Button(frameMain, text="Accéder (point 5)").grid(column=5, row=5)
+
+    #import image
+    image_button_exit = PhotoImage(file="ressources/images/exit2.png")
+    #resize
+    image_button_exit_photo = image_button_exit.subsample(6,6)
+
+    Button(frameMain,text="Exit", image=image_button_exit_photo, command=WindowMain.destroy).grid(column=5, row=6)
+    
+    #ajout des points dans le menu principal
+    frameMenuPoint1.grid(column=0, row=1)
+    frameMenuPoint2.grid(column=0, row=2)
+    frameMenuPoint3.grid(column=0, row=3)
+    frameMenuPoint4.grid(column=0, row=4)
+    frameMenuPoint5.grid(column=0, row=5)
+    
+    #ajout du menu principal
+    frameMain.grid()
 
 
-ttk.Label(frm, text="2. Trouvez les informations de l'adresse IP\n     A. Adresse de réseau \n     B. Adresse de broadcast \n     C. Adresse de sous-réseau").grid(column=0, row=3)
-ttk.Button(frm, text="Valider (point 2)", ).grid(column=2, row=3)
+#création et configurationd de la menubarre
+menubar = Menu(WindowMain)
 
+menu1 = Menu(menubar, tearoff=0)
+menu1.add_command(label="Accueil", command=displayMenuMain)
+menu1.add_command(label="Point1", command=displayPoint1)
+menu1.add_command(label="Point 2", command=displayPoint1)
+menu1.add_command(label="Point 3", command=displayPoint1)
+menu1.add_command(label="Point 4", command=displayPoint1)
+menu1.add_command(label="Point 5", command=displayPoint1)
+menu1.add_separator()
+menu1.add_command(label="Quitter", command=WindowMain.quit)
+menubar.add_cascade(label="Menu", menu=menu1)
 
-ttk.Label(frm, text="3. L'IP apartient-il au réseau ?").grid(column=0, row=5)
-ttk.Button(frm, text="Valider (point 3)", ).grid(column=2, row=5)
+menu2 = Menu(menubar, tearoff=0)
+menu2.add_command(label="A propos", command=displayAbout)
+menubar.add_cascade(label="Info", menu=menu2)
 
+WindowMain.config(menu=menubar)
 
-ttk.Label(frm, text="4. Sont-ils dans le même réseau ?").grid(column=0, row=7)
-ttk.Button(frm, text="Valider (point 4)", ).grid(column=2, row=7)
-
-ttk.Label(frm, text="5. Déterminer les possibilités\n     A. Le nombre d'hôtes \n     B. Découpe sur nombre de sous-réseaux \n     C. Découpe par nombre d'adresse IP").grid(column=0, row=9)
-ttk.Button(frm, text="Valider (point 5)", ).grid(column=2, row=9)
-
-
-ttk.Button(frm, text="Fermer", command=root.destroy).grid(column=1, row=11)
-
+displayMenuMain()
 #afficher la fenêtre 
-root.mainloop()
+WindowMain.mainloop()
+
