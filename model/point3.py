7adf057460381e23b23bae1cbf15517b501490d4
Alex <alexnoel218@gmail.com>
2022-09-28 12:59:47 +0200

    Implémentation point 3 dans interface graphique

--- model/point3.py
+++ model/point3.py
@@ -1,88 +1,89 @@
 from math import floor
 
-from functions import *
+from util.functions import *
 
 #----------------------------------------------------------------
 # Demande des informations
-ip = input("IP : ")
-masque = input("Masque : ")
-adresse_reseau_a_appartenir = input("Adresse réseau : ")
+# ip = input("IP : ")
+# masque = input("Masque : ")
+# adresse_reseau_a_appartenir = input("Adresse réseau : ")
 
-adresse_masque_valide = False
-adresse_ip_valide = False
-adresse_reseau_a_appartenir_valide = False
+def genererPoint3(IpAdress, MaskAdress, IpNetworkAdress):
+    adresse_masque_valide = False
+    adresse_ip_valide = False
+    adresse_reseau_a_appartenir_valide = False
 
-while not adresse_masque_valide or not adresse_ip_valide or not adresse_reseau_a_appartenir_valide:
-    # Séparation des octets dans une liste
-    liste_octet_ip = ip.split(".")
-    liste_octet_masque = masque.split(".")
-    liste_octet_adresse_reseau_a_appartenir = adresse_reseau_a_appartenir.split(".")
-    
-    # Vérification adresse ip
-    if verifyIsIpValid(ip):
-        adresse_ip_valide = True
+    while not adresse_masque_valide or not adresse_ip_valide or not adresse_reseau_a_appartenir_valide:
+        # Séparation des octets dans une liste
+        liste_octet_ip = IpAdress.split(".")
+        liste_octet_masque = MaskAdress.split(".")
+        liste_octet_adresse_reseau_a_appartenir = IpNetworkAdress.split(".")
+        
+        # Vérification adresse ip
+        if verifyIsIpValid(IpAdress):
+            adresse_ip_valide = True
+        else:
+            adresse_ip_valide = False
+
+        # Conversion de l'adresse ip de string en int
+        if(adresse_ip_valide == True):
+            liste_octet_ip_int = []
+            for i in liste_octet_ip:
+                # Ajout des octets en int dans une nouvelle liste
+                liste_octet_ip_int.append(int(i))
+
+        # Vérification adresse masque_classe
+        if verifyIsMaskValid(MaskAdress):
+            adresse_masque_valide = True
+        else:
+            adresse_masque_valide = False
+
+        # Conversion du masque de string en int
+        if(adresse_masque_valide == True):
+            liste_octet_masque_int = []
+            for i in liste_octet_masque:
+                # Ajout des octets en int dans une nouvelle liste
+                liste_octet_masque_int.append(int(i))
+
+        # Vérification adresse réseau à appartenir
+        if verifyIsIpValid(IpNetworkAdress):
+            adresse_reseau_a_appartenir_valide = True
+        else:
+            adresse_reseau_a_appartenir_valide = False
+
+        # Conversion de l'adresse réseau à appartenir de string en int
+        if(adresse_reseau_a_appartenir_valide == True):
+            liste_octet_reseau_a_appartenir_int = []
+            for i in liste_octet_adresse_reseau_a_appartenir:
+                # Ajout des octets en int dans une nouvelle liste
+                liste_octet_reseau_a_appartenir_int.append(int(i))
+
+        # On redemande l'adresse ip si elle n'est pas valide
+        if adresse_ip_valide == False:
+            print("Adresse ip n'est pas valide.")
+            return "IpInvalid"
+
+        # On redemande l'adresse du masque_classe si elle n'est pas valide
+        if adresse_masque_valide == False:
+            print("Adresse du masque n'est pas valide.")
+            return "MaskInvalid"
+
+        # On redemande l'adresse réseau à appartenir si elle n'est pas valide
+        if adresse_reseau_a_appartenir_valide == False:
+            print("Adresse réseau n'est pas valide.")
+            return "IpInvalid"
+
+    # Ajout de chaque octet de l'adresse ip en binaire dans une nouvelle liste
+    liste_binary_ip = [int_to_binary(i) for i in liste_octet_ip_int]
+    # Ajout de chaque octet de l'adresse de masque en binaire dans une nouvelle liste
+    liste_binary_masque = [int_to_binary(i) for i in liste_octet_masque_int]
+    # Calcul de l'adresse de reseau et de broadcast
+    binary_reseau_adresse, binary_broadcast_adresse = calcul_reseau_bc(liste_binary_ip,liste_binary_masque)
+
+    # Conversion des adresse de réseau et de broadcast en entier
+    liste_octet_reseau_int = [octet_to_int(binary_reseau_adresse[i]) for i in range(4)]
+
+    if(liste_octet_reseau_a_appartenir_int == liste_octet_reseau_int):
+        return "L'adresse IP " + IpAdress + " appartient bien au réseau " + IpNetworkAdress
     else:
-        adresse_ip_valide = False
-
-    # Conversion de l'adresse ip de string en int
-    if(adresse_ip_valide == True):
-        liste_octet_ip_int = []
-        for i in liste_octet_ip:
-            # Ajout des octets en int dans une nouvelle liste
-            liste_octet_ip_int.append(int(i))
-
-    # Vérification adresse masque_classe
-    if verifyIsMaskValid(masque):
-        adresse_masque_valide = True
-    else:
-        adresse_masque_valide = False
-
-    # Conversion du masque de string en int
-    if(adresse_masque_valide == True):
-        liste_octet_masque_int = []
-        for i in liste_octet_masque:
-            # Ajout des octets en int dans une nouvelle liste
-            liste_octet_masque_int.append(int(i))
-
-    # Vérification adresse réseau à appartenir
-    if verifyIsIpValid(adresse_reseau_a_appartenir):
-        adresse_reseau_a_appartenir_valide = True
-    else:
-        adresse_reseau_a_appartenir_valide = False
-
-    # Conversion de l'adresse réseau à appartenir de string en int
-    if(adresse_reseau_a_appartenir_valide == True):
-        liste_octet_reseau_a_appartenir_int = []
-        for i in liste_octet_adresse_reseau_a_appartenir:
-            # Ajout des octets en int dans une nouvelle liste
-            liste_octet_reseau_a_appartenir_int.append(int(i))
-
-    # On redemande l'adresse ip si elle n'est pas valide
-    if adresse_ip_valide == False:
-        print("Adresse ip n'est pas valide.")
-        ip = input("Rentrez une adresse ip valide : ")
-
-    # On redemande l'adresse du masque_classe si elle n'est pas valide
-    if adresse_masque_valide == False:
-        print("Adresse du masque n'est pas valide.")
-        masque = input("Rentrez une adresse de masque valide : ")
-
-    # On redemande l'adresse réseau à appartenir si elle n'est pas valide
-    if adresse_reseau_a_appartenir_valide == False:
-        print("Adresse réseau n'est pas valide.")
-        adresse_reseau_a_appartenir = input("Rentrez une adresse réseau valide : ")
-
-# Ajout de chaque octet de l'adresse ip en binaire dans une nouvelle liste
-liste_binary_ip = [int_to_binary(i) for i in liste_octet_ip_int]
-# Ajout de chaque octet de l'adresse de masque en binaire dans une nouvelle liste
-liste_binary_masque = [int_to_binary(i) for i in liste_octet_masque_int]
-# Calcul de l'adresse de reseau et de broadcast
-binary_reseau_adresse, binary_broadcast_adresse = calcul_reseau_bc(liste_binary_ip,liste_binary_masque)
-
-# Conversion des adresse de réseau et de broadcast en entier
-liste_octet_reseau_int = [octet_to_int(binary_reseau_adresse[i]) for i in range(4)]
-
-if(liste_octet_reseau_a_appartenir_int == liste_octet_reseau_int):
-    print("L'adresse IP ", ip," appartient bien au réseau ",adresse_reseau_a_appartenir)
-else:
-    print("L'adresse IP ", ip," n'appartient pas au réseau ",adresse_reseau_a_appartenir)
+        return "L'adresse IP " + IpAdress + " n'appartient pas au réseau " + IpNetworkAdress
--- views/viewPoint3.py
+++ views/viewPoint3.py
@@ -1,23 +1,49 @@
 from tkinter import *
+from tkinter import messagebox
+
+from model.point3 import genererPoint3
 
 
 def displayMenuThree(WindowMain,framePoint3):
+
+    global valueLabel
+    
+    valueAdresse = StringVar()
+    valueMasque = StringVar()
+    valueAdresseReseau = StringVar()
+    valueLabel = StringVar()
+
     #changement titre
     WindowMain.title("Projet LABO_TCPIP - Point 3")
     
     #création des composants
     Label(framePoint3, text="Veuillez encoder l'adresse IP", font=("Impact",15),foreground="white", background="#009790").grid(column=2, row=1)
-    Entry(framePoint3).grid(column=2, row=2, pady=15)
+    Entry(framePoint3,textvariable=valueAdresse).grid(column=2, row=2, pady=15)
     
     
     Label(framePoint3, text="Veuillez encoder le masque de l'adrese IP ", font=("Impact",15),foreground="white", background="#009790").grid(column=2, row=3)
-    Entry(framePoint3).grid(column=2, row=4, pady=15)
+    Entry(framePoint3,textvariable=valueMasque).grid(column=2, row=4, pady=15)
     
     Label(framePoint3, text="Le réseau dont vous voulez vérifier l'appartenance ", font=("Impact",15),foreground="white", background="#009790").grid(column=2, row=5)
-    Entry(framePoint3).grid(column=2, row=6, pady=15)
+    Entry(framePoint3,textvariable=valueAdresseReseau).grid(column=2, row=6, pady=15)
+
+    Label(framePoint3, textvariable=valueLabel, font=("Impact",10),foreground="white", background="#009790").grid(column=2, row=5)
     
-    Button(framePoint3, text="Vérifier la correspondance").grid(column=2, row=7, padx=200, pady=30, ipadx=50)
+    
+    Button(framePoint3, text="Vérifier la correspondance",command=lambda:genererInformationsThree(valueAdresse.get(),valueMasque.get(),valueAdresseReseau.get())).grid(column=2, row=7, padx=200, pady=30, ipadx=50)
     
     
     #ajout du conteneur
     framePoint3.grid()
+
+def genererInformationsThree(IpAdress, MaskAdress, IpNetworkAdress):
+    
+    result = genererPoint3(IpAdress, MaskAdress, IpNetworkAdress)
+    
+    #traitement
+    if((result == "IpInvalid")):
+        messagebox.showerror("ERREUR", "L'adrese IP encodée n'est pas valide\nmerci de d'en choisir une autre !")
+    elif((result == "MaskInvalid")):
+        messagebox.showerror("ERREUR", "L'adrese de masque encodée n'est pas valide\nmerci de d'en choisir une autre !")
+    else:
+        valueLabel.set("Resultats\n---------------------------------------------\n"+result)  
