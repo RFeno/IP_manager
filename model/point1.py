5ac5ac4f28e5287b44fa87d3681a450c975912d9
Adamaheto Saviour <102219621+RFeno@users.noreply.github.com>
2022-10-06 21:54:19 +0200

    ajout double retour à la ligne

--- model/point1.py
+++ model/point1.py
@@ -30,22 +30,22 @@ def genererPoint1(adresseIP):
         return (f'Classe {result[5][1]}')
     elif(firstByte < 127):
         #classe A
-        return (f'Classe {result[0][1]}: \n{result[0][2]} réseaux de {result[0][3]} machines')
+        return (f'Classe {result[0][1]}: \n\n{result[0][2]} réseaux de {result[0][3]} machines')
     elif(firstByte < 128):
         #classe reservées (A)
         return (f'Classe {result[5][1]}')
     elif(firstByte < 192):
         #classe B
-        return (f'Classe {result[1][1]}: \n{result[1][2]} réseaux de {result[1][3]} machines')
+        return (f'Classe {result[1][1]}: \n\n{result[1][2]} réseaux de {result[1][3]} machines')
     elif(firstByte < 224):
         #classe C
-        return (f'Classe {result[2][1]}: \n{result[2][2]} réseaux de {result[2][3]} machines')
+        return (f'Classe {result[2][1]}: \n\n{result[2][2]} réseaux de {result[2][3]} machines')
     elif(firstByte < 240):
         #classe D
-        return (f'Classe {result[3][1]}: \n{result[3][2]}')
+        return (f'Classe {result[3][1]}: \n\n{result[3][2]}')
     else:
         #classe E
-        return (f'Classe {result[4][1]}: \n{result[4][2]}')
+        return (f'Classe {result[4][1]}: \n\n{result[4][2]}')
 
 
 
