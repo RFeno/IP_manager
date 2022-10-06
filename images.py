dc214e5933e5f883acfc201c8440aecf938586dc
Adamaheto Saviour <102219621+RFeno@users.noreply.github.com>
2022-10-06 21:56:15 +0200

    ajout du fichiers comportant les images

---
+++ images.py
@@ -0,0 +1,28 @@
+from tkinter import PhotoImage, Tk
+
+image_button_connexion = PhotoImage(
+    file=("ressources/images/viewsIMG/connexion.png"))
+
+image_button_acd1 = PhotoImage(
+    file=("ressources/images/viewsIMG/acd1.png"))
+
+image_button_acd2 = PhotoImage(
+    file=("ressources/images/viewsIMG/acd2.png"))
+
+image_button_acd3 = PhotoImage(
+    file=("ressources/images/viewsIMG/acd3.png"))
+
+image_button_acd4 = PhotoImage(
+    file=("ressources/images/viewsIMG/acd4.png"))
+
+image_button_acd5 = PhotoImage(
+    file=("ressources/images/viewsIMG/acd5.png"))
+
+image_button_quitter= PhotoImage(
+    file=("ressources/images/viewsIMG/quitter.png"))
+
+image_button_generer = PhotoImage(
+    file=("ressources/images/viewsIMG/generer.png"))
+
+image_button_retour = PhotoImage(
+    file=("ressources/images/viewsIMG/retour_au_menu.png"))
