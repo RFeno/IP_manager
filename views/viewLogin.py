8dd71d3b69babedcc303e129e5561f453cb023ae
Adamaheto Saviour <102219621+RFeno@users.noreply.github.com>
2022-10-06 21:55:34 +0200

    ajout de l'interface de connexion V2

--- views/viewLogin.py
+++ views/viewLogin.py
@@ -1,10 +1,19 @@
-from tkinter import *
+from importlib.metadata import entry_points
 from tkinter import messagebox
 
 from util.checkUser import checkUserPassword
-from views.menu import displayMenuMain 
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import PhotoImage, Tk
 
-def DisplayLogin(WindowMain,frameLogin,frameMain,framePoint1,framePoint2,framePoint3,framePoint4,framePoint5):
+from images import *
+# from tkinter import *
+# Explicit imports to satisfy Flake8
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+
+from views.menu import displayMenuMain
+
+def DisplayLogin(WindowMain):
     #changement titre
     WindowMain.title("Projet LABO_TCPIP - Connexion")
     
@@ -12,23 +21,118 @@ def DisplayLogin(WindowMain,frameLogin,frameMain,framePoint1,framePoint2,framePo
     usernamevalue = StringVar()
     passwordvalue = StringVar()
     
-    Label(frameLogin, text="Nom d'utilisateur", font=("Impact",15),foreground="white", background="#009790").grid(column=2, row=2,padx=250)
-    Entry(frameLogin, textvariable=usernamevalue).grid(column=2, row=3, pady=15, padx=15)
+    #global pour êtres appelés dans d'autres méthodes
+    global canvas
+    global entry_username
+    global entry_password
+    global button_Create
+    canvas = Canvas(
+    WindowMain,
+    bg = "#A7D7C5",
+    height = 600,
+    width = 800,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+    )
+
+    canvas.place(x = 0, y = 0)
+
+    canvas.create_rectangle(
+        173.0,
+        79.0,
+        628.0,
+        534.0,
+        fill="#F6FBF9",
+        outline="")
+
+    canvas.create_text(
+        309.0,
+        116.0,
+        anchor="nw",
+        text="Connexion",
+        fill="#212B27",
+        font=("Karla Bold", 36 * -1)
+    )
+
+    entry_username = Entry(
+        textvariable=usernamevalue,
+        bd=0,
+        bg="#FFFFFF",
+        highlightthickness=0
+    )
     
-    Label(frameLogin, text="Mot de passe", font=("Impact",15),foreground="white", background="#009790").grid(column=2, row=4)
-    Entry(frameLogin, textvariable=passwordvalue,show="*").grid(column=2, row=5, pady=15,padx=15)
+    entry_username.place(
+        x=222.0,
+        y=238.0,
+        width=355.0,
+        height=53.0
+    )
+
+    entry_password = Entry(
+        show="*",
+        bd=0,
+        bg="#FFFFFF",
+        highlightthickness=0,
+        textvariable=passwordvalue
+    )
+    entry_password.place(
+        x=225.0,
+        y=318.0,
+        width=355.0,
+        height=53.0
+    )
+
+    canvas.create_text(
+        215.0,
+        212.0,
+        anchor="nw",
+        text="Nom d’utilisateur",
+        fill="#000000",
+        font=("Karla Regular", 15 * -1)
+    )
+
+    canvas.create_text(
+        215.0,
+        292.0,
+        anchor="nw",
+        text="Mot de passe",
+        fill="#000000",
+        font=("Karla Regular", 15 * -1)
+    )
+
     
-    Button(frameLogin, text="Connexion", command=lambda:connexion(usernamevalue.get(),passwordvalue.get(),WindowMain,frameMain,frameLogin,framePoint1,framePoint2,framePoint3,framePoint4,framePoint5)).grid(column=2, row=9, pady=30, ipadx=50)
+    button_Create = Button(
+        text="Créer le compte",
+        image=image_button_connexion,
+        borderwidth=0,
+        highlightthickness=0,
+        relief="flat",
+        command=lambda:connexion(usernamevalue.get(),passwordvalue.get(),WindowMain,)
+    )
+    
+    button_Create.place(
+        x=273.0,
+        y=384.0,
+        width=255.0,
+        height=59.0
+    )
     
     #ajout du conteneur
-    frameLogin.grid(stick=E)
     
     
-def connexion(username,password,WindowMain,frameMain,frameLogin,framePoint1,framePoint2,framePoint3,framePoint4,framePoint5):
+    
+def connexion(username,password,WindowMain):
     if(checkUserPassword(username, password)):
         print("identifiants valides")
-        frameLogin.grid_forget()
-        displayMenuMain(WindowMain,frameMain,framePoint1,framePoint2,framePoint3,framePoint4,framePoint5)
+        
+        #suprression de l'ancienne fenêtre
+        canvas.destroy()
+        button_Create.place_forget()
+        entry_password.place_forget()
+        entry_username.place_forget()
+        
+        displayMenuMain(WindowMain)
     else:
         print("identifiants invalides")
         messagebox.showerror("ERREUR", "Les identifiants sont invalides")
